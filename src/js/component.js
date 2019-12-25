5d5efa82e423b253ccbf56bd01a4d441e309e738
Andrii <litvin.andriy91@gmail.com>
2019-12-25 17:14:45 +0200

    add prodj

---
+++ src/js/component.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    return $('nav').toggleClass("fixed", $(window).scrollTop() > 0);
+  });
+
+  $('.scroll').click(function (e) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+
+    $('body,html').animate({
+      scrollTop: top - 80
+    }, 1500);
+
+  });
+
+
+
+
+  if ($(window).width() < 1200) {
+    $('.partners-list, .header-logo').addClass('marquee');
+    $('.marquee').marquee({
+      duration: 15000,
+      startVisible: true,
+      duplicated: true
+    });
+
+    $('.reviews_one-item').each(function (index) {
+      $(this).attr('href', 'images/' + (index + 1) + 'a.jpg');
+    })
+
+  }
+
+  $('.reviews_one-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoHeight: true,
+    items: 1
+  });
+
+  $('.reviews_two-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 42,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      1000: {
+        items: 2
+      }
+    }
+  })
+
+  $('.faq-item').click(function () {
+    $(this).toggleClass('active');
+    $(this).find('.more').slideToggle(300);
+  })
+
+  $('nav').hover(function () {
+    $('img[data-src]').each(function () {
+      $(this).attr('src', $(this).data('src'));
+    })
+  })
+
+  $('.mob-btn').click(function () {
+    $('.menu').slideToggle(300);
+    $('img[data-src]').each(function () {
+      $(this).attr('src', $(this).data('src'));
+    })
+  })
+
+  var observer = lozad();
+  observer.observe();
+});
